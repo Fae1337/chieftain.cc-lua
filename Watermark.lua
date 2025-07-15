e300ae1875b283d7c0bd4f30c32f01e924e0c106
Failure-user <157841614+Fae1337@users.noreply.github.com>
2025-07-15 11:55:46 +0300

    Update Watermark.lua

--- Watermark.lua
+++ Watermark.lua
@@ -1,17 +1,23 @@
 hook.Add("HUDPaint", "CustomWatermark", function()
+    surface.CreateFont("Trebuchet20", {
+        font = "Trebuchet MS",
+        size = 25,-- свой размер
+        weight = 500,
+    })
+
     local scrW, scrH = ScrW(), ScrH()
 
     local steamName = LocalPlayer():Nick()
     local fps = math.Round(1 / FrameTime())
     local ping = LocalPlayer():Ping()
-    -- local serverIP = game.GetIPAddress() or "N/A"
+    -- local serverIP = game.GetIPAddress() --
 
     local timeStr = os.date("%H:%M:%S")
 
     local prefix = "Chieftain.cc"
-    local rest = string.format(" | %s | FPS: %d | Ping: %d ms | Time: %s", steamName, fps, ping, timeStr)
+    local rest = string.format(" | %s | FPS: %d | %d ms | %s", steamName, fps, ping, timeStr)
 
-    surface.SetFont("Trebuchet24")
+    surface.SetFont("Trebuchet20")
     local prefixW, prefixH = surface.GetTextSize(prefix)
     local restW, restH = surface.GetTextSize(rest)
     local totalW = prefixW + restW
@@ -19,8 +25,6 @@ hook.Add("HUDPaint", "CustomWatermark", function()
     surface.SetDrawColor(0, 0, 0, 235)
     surface.DrawRect(scrW - totalW - 24, 5, totalW + 16, prefixH + 5)
 
-    -- Рисуем зелёный префикс
-    draw.SimpleText(prefix, "Trebuchet24", scrW - totalW - 16, 8, Color(50, 205, 50, 255), TEXT_ALIGN_LEFT, TEXT_ALIGN_TOP)
-    -- Рисуем остальной текст белым
-    draw.SimpleText(rest, "Trebuchet24", scrW - totalW - 16 + prefixW, 8, Color(255, 255, 255, 255), TEXT_ALIGN_LEFT, TEXT_ALIGN_TOP)
+    draw.SimpleText(prefix, "Trebuchet20", scrW - totalW - 16, 8, Color(50, 205, 50, 255), TEXT_ALIGN_LEFT, TEXT_ALIGN_TOP)
+    draw.SimpleText(rest, "Trebuchet20", scrW - totalW - 16 + prefixW, 8, Color(255, 255, 255, 255), TEXT_ALIGN_LEFT, TEXT_ALIGN_TOP)
 end)
